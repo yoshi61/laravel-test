ce30ed01c29024646c9c6c1c6ba6d9e98e84f652
yoshi61 <mj81hideki@gmail.com>
2019-03-29 15:01:28 +1030

    ok

--- tv9-scraping/save_article.py
+++ tv9-scraping/save_article.py
@@ -4,22 +4,21 @@ import pymysql.cursors
 from config import database
 import helper
 
-#サーバー接続
+# database connection
 connection = helper.connectDB(database)
 
-#チャンネルネコをデーターベースに保存するためのオブジェクト。
 class SaveArticle:
 	def __init__(self):
 		print("'saving' object created...")
 
 	def saveArticle(self, article):
-		"""Validation　idが無ければそのまま保存する　あればデーターを更新する"""
+		"""check if article is already in database"""
 		with connection.cursor() as cursor:
 			cursor.execute("SELECT id FROM articles WHERE article_id = " + str(article.id))
 			result = cursor.fetchall()
 
 		if(len(result) == 0):
-			"""データーベースになかった場合は追加する"""
+			"""If article not in database, save it"""
 			with connection.cursor() as cursor:
 				cursor.execute("INSERT INTO `" + database['db'] + "`.`articles` (`article_id`, `title`, `page_url`, `pic_url`, `intro`) VALUES (%s, %s, %s, %s, %s)", (article.id, article.pageTitleStr, article.pageUrl, article.imgUrl, article.pageIntro))
 				connection.commit()
